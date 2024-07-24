b9c58b084814ae4fb8f99f2c0376deb049f95ba1
gigajdn <129200971+gigajdn@users.noreply.github.com>
2024-07-24 15:37:34 +0700

    Update adminRoutes.js

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const adminController = require('../controllers/adminController');
 const { isAuthenticated, isAdmin } = require('../middlewares/authMiddleware');
 
+//isAuthenticated hanya mengecek apakah pengguna mempunyai akses untuk masuk kedalam sistem atau tidak
+//isAdmin akan melakukan pengecekan lebih terhadap role pengguna apakah role pengguna tersebut merupakan admin
 router.post('/importAlumniData', isAuthenticated, isAdmin, adminController.importAlumniData);
 router.get('/manageAlumniData', isAuthenticated, isAdmin, adminController.manageAlumniData);
 router.post('/createReport', isAuthenticated, isAdmin, adminController.createReport);
