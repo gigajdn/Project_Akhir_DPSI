28d4dd5d0eac2de089912734dab4559148d17b37
gigajdn <129200971+gigajdn@users.noreply.github.com>
2024-07-24 15:38:18 +0700

    Update historyRoutes.js

--- routes/historyRoutes.js
+++ routes/historyRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const historyController = require('../controllers/historyController');
 const { isAuthenticated } = require('../middlewares/authMiddleware');
 
+//isAuthenticated hanya mengecek apakah pengguna mempunyai akses untuk masuk kedalam sistem atau tidak
 router.get('/history/:alumniId', isAuthenticated, historyController.getHistoryByAlumniId);
 router.post('/history', isAuthenticated, historyController.addHistoryEntry);
 
