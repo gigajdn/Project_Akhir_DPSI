49227d327019723300ea7560ec76993ca804d2bb
gigajdn <129200971+gigajdn@users.noreply.github.com>
2024-07-24 15:37:53 +0700

    Update alumniRoutes.js

--- routes/alumniRoutes.js
+++ routes/alumniRoutes.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const alumniController = require('../controllers/alumniController');
 const { isAuthenticated, isAlumni } = require('../middlewares/authMiddleware');
 
+//isAuthenticated hanya mengecek apakah pengguna mempunyai akses untuk masuk kedalam sistem atau tidak
+//isAdmin akan melakukan pengecekan lebih terhadap role pengguna apakah role pengguna tersebut merupakan alumni
 router.get('/profile/:id', isAuthenticated, isAlumni, alumniController.viewProfile);
 router.put('/profile/:id', isAuthenticated, isAlumni, alumniController.updateProfile);
 router.get('/accessAlumniInfo', isAuthenticated, isAlumni, alumniController.accessAlumniInfo);
