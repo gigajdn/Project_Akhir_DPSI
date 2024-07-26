b7c6bbfc96b22abdb43da2f3f04e1f2bbeabb492
gigajdn <giga2100016020@webmail.uad.ac.id>
2024-07-26 14:37:10 +0700

    update work history models

--- models/schemas/workHistorySchema.js
+++ models/schemas/workHistorySchema.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const workHistorySchema = new mongoose.Schema({
   company: { type: String, default: '' },
   position: { type: String, default: '' },
-  startDate: { type: Date, default: '00-000-0-0-0' },
-  endDate: { type: Date, default: '00-000-0-0-0' },
+  startDate: { type: Date, default: Date.now },
+  endDate: { type: Date, default: Date.now },
   responsibilities: { type: String, default: '' },
 }, { timestamps: true });
 
