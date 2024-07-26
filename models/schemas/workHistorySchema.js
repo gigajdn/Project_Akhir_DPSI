7b04d1d38226f0c6b05e2eb81853de6e22c00dbd
gigajdn <giga2100016020@webmail.uad.ac.id>
2024-07-26 14:34:18 +0700

    update alumni models

--- models/schemas/workHistorySchema.js
+++ models/schemas/workHistorySchema.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const workHistorySchema = new mongoose.Schema({
   company: { type: String, default: '' },
   position: { type: String, default: '' },
-  startDate: { type: Date, default: new Date(0) },
-  endDate: { type: Date, default: new Date(0) },
+  startDate: { type: Date, default: '00-000-0-0-0' },
+  endDate: { type: Date, default: '00-000-0-0-0' },
   responsibilities: { type: String, default: '' },
 }, { timestamps: true });
 
